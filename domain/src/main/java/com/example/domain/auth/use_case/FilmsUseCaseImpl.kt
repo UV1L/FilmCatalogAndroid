5dcce7605879011d7fad89784a960c9ef5072ee9
UV1L <50074365+UV1L@users.noreply.github.com>
2021-12-16 22:32:23 +0500

    dagger changes

--- domain/src/main/java/com/example/domain/auth/use_case/FilmsUseCaseImpl.kt
+++ domain/src/main/java/com/example/domain/auth/use_case/FilmsUseCaseImpl.kt
@@ -4,8 +4,9 @@ import com.example.domain.auth.entities.Film
 import com.example.domain.auth.entities.Resource
 import com.example.domain.auth.repo.FilmsRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class FilmsUseCaseImpl(private val filmsRepository: FilmsRepository) : FilmsUseCase {
+class FilmsUseCaseImpl @Inject constructor(private val filmsRepository: FilmsRepository) : FilmsUseCase {
 
     override suspend fun getFilms(token: String): Flow<Resource<List<Film>>> {
 
